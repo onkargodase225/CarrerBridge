e37306600f8c4aaf3002d0f36abce29f321f4ef8
Onkar Godase <onkarlgodase225@gmail.com>
2025-09-23 14:54:01 +0530

    Configured frontend to use environment variables

--- frontend2/src/utils/constant.js
+++ frontend2/src/utils/constant.js
@@ -1,6 +1,12 @@
 
-//  bakend ka url
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user"; 
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+// //  bakend ka url
+// export const USER_API_END_POINT="http://localhost:8000/api/v1/user"; 
+// export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
+// export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
+// export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+
+
+export const USER_API_END_POINT = import.meta.env.VITE_USER_API_URL;
+export const JOB_API_END_POINT = import.meta.env.VITE_JOB_API_URL;
+export const APPLICATION_API_END_POINT = import.meta.env.VITE_APPLICATION_API_URL;
+export const COMPANY_API_END_POINT = import.meta.env.VITE_COMPANY_API_URL;
